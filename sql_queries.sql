44a0e1129a55b6ce6a22981bc23c10d98dfa1f6b
vaibhav <vaibhavsaner@gmail.com>
2026-09-03 19:43:51 +0530

    consecutive group

--- sql_queries.sql
+++ sql_queries.sql
@@ -54,3 +54,71 @@ where salary>avg_salary;
 -- Q4- 
 
 select * from employee;
+
+CREATE TABLE customer_event (
+    customerid INT,
+    event_type VARCHAR(20),
+    event_timestamp TIMESTAMP
+);
+
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (101, 'login', '2026-08-09 09:00:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (101, 'order_purchased', '2026-08-09 09:05:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (101, 'logout', '2026-08-09 09:10:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (102, 'login', '2026-08-09 11:00:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (102, 'logout', '2026-08-09 10:00:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (103, 'login', '2026-08-09 10:07:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (103, 'logout', '2026-08-09 10:15:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (104, 'login', '2026-08-09 11:10:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (104, 'logout', '2026-08-09 11:18:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (105, 'login', '2026-08-09 11:30:00');
+
+INSERT INTO customer_event (customerid, event_type, event_timestamp)
+VALUES (105, 'order_purchased', '2026-08-09 11:35:00');
+
+
+select * from customer_event;
+
+-- customers who made order after purchased
+select DISTINCT l.customerid,p.event_timestamp from customer_event l  
+join customer_event p 
+on l.customerid=p.customerid
+WHERE l.event_type='login' and p.event_type='order_purchased'
+and p.event_timestamp>l.event_timestamp;
+
+-- another approach 
+
+WITH ordered_events AS (
+    SELECT
+        customerid,
+        event_type,
+        event_timestamp,
+        LAG(event_type) OVER (
+            PARTITION BY customerid
+            ORDER BY event_timestamp
+        ) AS previous_event
+    FROM customer_event
+)
+SELECT customerid,event_timestamp
+FROM ordered_events
+WHERE event_type = 'order_purchased'
+  AND previous_event = 'login';
